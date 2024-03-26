845707230a90cc5dc3b048147911f5744ddb1736
Dorian JAMES <dorianthelycan@gmail.com>
2024-03-26 15:35:18 +0300

    .aap and .apk outputs taken.

---
+++ Assets/Scripts/Ads/InGameAdManager.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+
+public class InGameAdManager : MonoBehaviour
+{
+    // These ad units are configured to always serve test ads.
+#if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-6309338851156090/3222173449";
+#elif UNITY_IPHONE
+  private string _adUnitId = "ca-app-pub-6309338851156090/2148749013";
+#else
+  private string _adUnitId = "unused";
+#endif
+
+    BannerView _bannerView;
+    
+    
+    public void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            // This callback is called once the MobileAds SDK is initialized.
+        });
+        
+        LoadAd();
+    }
+    
+    /// <summary>
+    /// Creates a 320x50 banner view at top of the screen.
+    /// </summary>
+    public void CreateBannerView()
+    {
+        Debug.Log("Creating banner view");
+
+        // If we already have a banner, destroy the old one.
+        if (_bannerView != null)
+        {
+            _bannerView.Destroy();
+        }
+
+        // Create a 320x50 banner at top of the screen
+        _bannerView = new BannerView(_adUnitId, AdSize.IABBanner, 0,70);
+    }
+    
+    public void LoadAd()
+    {
+        // create an instance of a banner view first.
+        if(_bannerView == null)
+        {
+            CreateBannerView();
+        }
+
+        // create our request used to load the ad.
+        var adRequest = new AdRequest();
+
+        // send the request to load the ad.
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
+    }
+}

46fd8c933c2b57b56fc2db41bfb8ce32f88a78a2
Dorian JAMES <dorianthelycan@gmail.com>
2024-05-29 15:31:08 +0300

    IOS P.List Permision added. Android optimization completed.

--- Assets/Scripts/Ads/InGameAdManager.cs
+++ Assets/Scripts/Ads/InGameAdManager.cs
@@ -43,7 +43,7 @@ public class InGameAdManager : MonoBehaviour
         }
 
         // Create a 320x50 banner at top of the screen
-        _bannerView = new BannerView(_adUnitId, AdSize.IABBanner, 0,70);
+        _bannerView = new BannerView(_adUnitId, AdSize.IABBanner, 0,30);
     }
     
     public void LoadAd()
